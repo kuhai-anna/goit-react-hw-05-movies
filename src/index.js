cdfe6df1a4ab0aa58895d477dec2b0eebbd0c68b
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-16 00:53:21 +0300

    Add BrowserRouter to the index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'modern-normalize';
+import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyle.jsx';
 import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
 import { theme } from './constants/theme';
+import 'modern-normalize';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter basename="/goit-react-hw-05-movies">
+        <App />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   </React.StrictMode>
