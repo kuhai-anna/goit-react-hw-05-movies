ae4f471cdc365bc74ad14d1e5838bc07a7708880
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-16 01:00:50 +0300

    Add Home and Movie pages to the App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import Home from 'pages/Home';
+import Movies from 'pages/Movies';
+import { Routes, Route, NavLink } from 'react-router-dom';
+// import { Section } from './Section/Section';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/movies">Movies</NavLink>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<div>Movie details</div>} />
+        <Route path="/movies/:movieId/cast" element={<div>Cast</div>} />
+        <Route path="/movies/:movieId/reviews" element={<div>Reviews</div>} />
+      </Routes>
+    </>
   );
 };
