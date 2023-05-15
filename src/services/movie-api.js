b17a8a985fe8ccf2ecfb7aee1c4824526c5d6071
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-15 22:53:12 +0300

    Create movie-api.js

---
+++ src/services/movie-api.js
@@ -0,0 +1,74 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const KEY = 'c87cc88294da77d4343c2f6d8278b44b';
+
+const fetchTrendingMovies = async page => {
+  const searchParams = new URLSearchParams({
+    api_key: `${KEY}`,
+    page: `${page}`,
+  });
+
+  const { data } = await axios.get(
+    `${BASE_URL}trending/all/day?${searchParams}`
+  );
+  return data;
+};
+
+const fetchMoviesWithQuery = async (searchQuery, page) => {
+  const searchParams = new URLSearchParams({
+    key: `${KEY}`,
+    language: 'en-US',
+    query: `${searchQuery}`,
+    include_adult: 'false',
+    page: `${page}`,
+  });
+
+  const { data } = await axios.get(`${BASE_URL}search/movie?${searchParams}`);
+  return data;
+};
+
+const fetchMovieDetails = async moviId => {
+  const searchParams = new URLSearchParams({
+    key: `${KEY}`,
+    language: 'en-US',
+  });
+
+  const { data } = await axios.get(
+    `${BASE_URL}movie/${moviId}?${searchParams}`
+  );
+  return data;
+};
+
+const fetchMovieCredits = async moviId => {
+  const searchParams = new URLSearchParams({
+    key: `${KEY}`,
+    language: 'en-US',
+  });
+
+  const { data } = await axios.get(
+    `${BASE_URL}movie/${moviId}/credits?${searchParams}`
+  );
+  return data;
+};
+
+const fetchMovieReviews = async moviId => {
+  const searchParams = new URLSearchParams({
+    key: `${KEY}`,
+    language: 'en-US',
+    page: '1',
+  });
+
+  const { data } = await axios.get(
+    `${BASE_URL}movie/${moviId}/reviews?${searchParams}`
+  );
+  return data;
+};
+
+export const api = {
+  fetchTrendingMovies,
+  fetchMoviesWithQuery,
+  fetchMovieDetails,
+  fetchMovieCredits,
+  fetchMovieReviews,
+};
