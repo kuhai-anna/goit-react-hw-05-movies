0c1f83e96385bf02a890bb6b7052c4495a35f51c
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-16 21:55:30 +0300

    Add new logic to Section components rendering

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,15 +1,47 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-import { Container, SectionWrapper } from './Section.styled';
+import {
+  Container,
+  SectionWrapper,
+  MainTitle,
+  SectionTitle,
+} from './Section.styled';
 
-export const Section = ({ children }) => {
-  return (
-    <SectionWrapper>
-      <Container>{children}</Container>
-    </SectionWrapper>
-  );
+export const Section = ({ tag, mainTitle, title, children }) => {
+  let sectionContent;
+
+  switch (tag) {
+    case 'header':
+      sectionContent = <header>{children}</header>;
+      break;
+    case 'main':
+      sectionContent = <main>{children}</main>;
+      break;
+    case 'footer':
+      sectionContent = <footer>{children}</footer>;
+      break;
+    default:
+      sectionContent = (
+        <SectionWrapper>
+          <Container>
+            {mainTitle ? (
+              <MainTitle>{mainTitle}</MainTitle>
+            ) : (
+              <SectionTitle>{title}</SectionTitle>
+            )}
+            {children}
+          </Container>
+        </SectionWrapper>
+      );
+      break;
+  }
+
+  return sectionContent;
 };
 
-// Section.propTypes = {
-//   children: PropTypes.any.isRequired,
-// };
+Section.propTypes = {
+  tag: PropTypes.string,
+  mainTitle: PropTypes.string,
+  title: PropTypes.string,
+  children: PropTypes.any.isRequired,
+};
