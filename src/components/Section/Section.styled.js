f5c3a3b6af8a05c69f5ea2f47ad22c27624fbcc5
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-16 21:50:38 +0300

    Add MainTitle and SectionTitle styled components
    Add the MainTitle and SectionTitle styled components to the Section.styled.js file

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -8,3 +8,19 @@ export const Container = styled.div`
   margin: 0 auto;
   padding: ${props => props.theme.spacing(0)} ${props => props.theme.spacing(4)};
 `;
+
+export const MainTitle = styled.h1`
+  margin-bottom: ${props => props.theme.spacing(5)};
+
+  font-size: 30px;
+  text-align: center;
+  color: ${props => props.theme.colors.text};
+`;
+
+export const SectionTitle = styled.h2`
+  margin-bottom: ${props => props.theme.spacing(4)};
+
+  font-size: 24px;
+  text-align: center;
+  color: ${props => props.theme.colors.text};
+`;
