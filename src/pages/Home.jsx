a8900ac7c233b8dfdd7f2b50903f51caf24bb9ed
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-16 19:28:19 +0300

    Add minimal logic to the Home page component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,17 @@
-const Home = () => {};
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  const movies = ['batman', 'duna', 'spiderman'];
+
+  return (
+    <ul>
+      {movies.map(movie => (
+        <li key={movie}>
+          <Link to={`/movies/${movie}`}>{movie}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default Home;
