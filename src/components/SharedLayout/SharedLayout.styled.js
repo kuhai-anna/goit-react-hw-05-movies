121cda7a5f1b1a13243d898ebb9b047a8bb59624
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-17 02:20:00 +0300

    Add styles for the header component

--- src/components/SharedLayout/SharedLayout.styled.js
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -1,6 +1,14 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
+export const HeaderWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Nav = styled.nav``;
+
 export const Link = styled(NavLink)`
   text-decoration: none;
 
