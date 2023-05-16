686ff0997b035a62f9df48277a0db41c8925777f
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-17 02:21:02 +0300

    Add new components to the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Section } from 'components/Section/Section';
+import { MovieGallery } from 'components/MovieGallery/MovieGallery';
 
 const Home = () => {
   const movies = ['batman', 'duna', 'spiderman'];
 
   return (
-    <ul>
-      {movies.map(movie => (
-        <li key={movie}>
-          <Link to={`/movies/${movie}`}>{movie}</Link>
-        </li>
-      ))}
-    </ul>
+    <Section mainTitle={'Tranding today'}>
+      <MovieGallery movies={movies} />
+    </Section>
   );
 };
 
