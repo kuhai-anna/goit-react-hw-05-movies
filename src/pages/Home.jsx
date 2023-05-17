7bfabd4036183c487d9bf0cb83c29c62ac4a1030
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-17 03:54:14 +0300

    Update Home.jsx
    1. Add useState and useEffect
    2. Add state machine
    3. Add new markup
    4. Add load more button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,67 @@
+import { useEffect, useState } from 'react';
 import { Section } from 'components/Section/Section';
 import { MovieGallery } from 'components/MovieGallery/MovieGallery';
+import { Status } from 'constants/status';
+import { api } from 'services/movie-api';
+import { Loader } from 'components/Loader/Loader';
+import { MovieGalleryErrorView } from 'components/MovieGallery/MovieGalleryErrorView/MovieGalleryErrorView';
+import { Button } from 'components/Button/Button';
 
 const Home = () => {
-  const movies = ['batman', 'duna', 'spiderman'];
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState();
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
+  const notLastPage = page < Math.ceil(totalPages / 20);
 
-  return (
-    <Section mainTitle={'Tranding today'}>
-      <MovieGallery movies={movies} />
-    </Section>
-  );
+  useEffect(() => {
+    async function getTrandingMovies() {
+      setStatus(Status.PENDING);
+      try {
+        const { results, total_pages } = await api.fetchTrendingMovies(page);
+
+        page === 1
+          ? setMovies(results)
+          : setMovies(prevState => [...prevState, ...results]);
+
+        setTotalPages(total_pages);
+        setStatus(Status.RESOLVED);
+      } catch (error) {
+        setError(error);
+        setStatus(Status.REJECTED);
+      }
+    }
+
+    getTrandingMovies();
+  }, [page]);
+
+  // збільшення номеру сторінки при кліку на кнопку
+  const loadMoreBtnClick = () => {
+    setPage(prevState => prevState + 1);
+  };
+
+  // рендер компонентів в залежності від статусу
+  if (status === Status.PENDING) {
+    return <Loader />;
+  }
+
+  if (status === Status.REJECTED) {
+    return (
+      <MovieGalleryErrorView
+        message={`Whoops, something went wrong. ${error.message}`}
+      />
+    );
+  }
+
+  if (status === Status.RESOLVED) {
+    return (
+      <Section mainTitle={'Tranding today'}>
+        <MovieGallery movies={movies} />
+        {notLastPage && <Button onClick={loadMoreBtnClick} />}
+      </Section>
+    );
+  }
 };
 
 export default Home;
