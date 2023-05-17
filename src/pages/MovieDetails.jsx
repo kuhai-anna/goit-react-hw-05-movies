7381c2f72d6f0e06af058ac2d65b6cfc7cc6e500
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-17 17:02:28 +0300

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,10 +1,13 @@
+import { Section } from 'components/Section/Section';
 import { Link, Outlet, useParams } from 'react-router-dom';
 
 const MovieDetails = () => {
-  // const { movieId } = useParams();
+  const { movieId } = useParams();
+  console.log(movieId);
 
   return (
-    <>
+    <Section>
+      <h3>Additional information</h3>
       <ul>
         <li>
           <Link to="cast">Cast</Link>
@@ -14,7 +17,7 @@ const MovieDetails = () => {
         </li>
       </ul>
       <Outlet />
-    </>
+    </Section>
   );
 };
 
