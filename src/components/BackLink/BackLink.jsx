441a1cebedfe113512465240a608df8ec9d4180f
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-19 01:32:02 +0300

    Create BackLink.jsx

---
+++ src/components/BackLink/BackLink.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { AiOutlineDoubleLeft } from 'react-icons/ai';
+
+export const BackLink = ({ to, children }) => {
+  return (
+    <Link to={to}>
+      <AiOutlineDoubleLeft size="16" />
+      {children}
+    </Link>
+  );
+};
+
+BackLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+};
