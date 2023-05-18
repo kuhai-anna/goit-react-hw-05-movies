58ad0b64b7bb402ece08ae58eddbfd258b7bc483
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-18 04:22:48 +0300

    Add return to the ReviewsItem

--- src/components/Reviews/ReviewsItem/ReviewsItem.jsx
+++ src/components/Reviews/ReviewsItem/ReviewsItem.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 
 export const ReviewsItem = ({ author, content }) => {
-  <li>
-    <p>Author: {author}</p>
-    <p>'{content}'</p>
-  </li>;
+  return (
+    <li>
+      <p>Author: {author}</p>
+      <p>'{content}'</p>
+    </li>
+  );
 };
 
 ReviewsItem.propTypes = {
