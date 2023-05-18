747d5c13de58a9faac9d366a74ee42e204961aaf
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-19 01:33:18 +0300

    Add useLocation

--- src/components/MovieGallery/MovieGalleryItem/MovieGalleryItem.jsx
+++ src/components/MovieGallery/MovieGalleryItem/MovieGalleryItem.jsx
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import { GalleryItem, MovieLink } from './MovieGalleryItem.styled';
 import poster from 'images/frame-neon.jpeg';
 
 export const MovieGalleryItem = ({ movieId, title, name, url }) => {
+  const location = useLocation();
   let movieTitle =
     title && title.length > 50 ? `${title.slice(0, 50)}...` : title;
   let movieName = name && name.length > 50 ? `${name.slice(0, 50)}...` : name;
 
   return (
     <GalleryItem>
-      <MovieLink to={`/movies/${movieId}`}>
+      <MovieLink to={`/movies/${movieId}`} state={{ from: location }}>
         <div>
           <img
             src={url ? `https://image.tmdb.org/t/p/w500/${url}` : poster}
