6902829793091acfe3663dff376576c8c7f3f61c
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-18 04:25:22 +0300

    Update options for rendering MovieGalleryItem

--- src/components/MovieGallery/MovieGalleryItem/MovieGalleryItem.jsx
+++ src/components/MovieGallery/MovieGalleryItem/MovieGalleryItem.jsx
@@ -1,20 +1,27 @@
 import PropTypes from 'prop-types';
 import { GalleryItem, MovieLink } from './MovieGalleryItem.styled';
+import poster from 'images/frame-neon.jpeg';
 
 export const MovieGalleryItem = ({ movieId, title, name, url }) => {
+  let movieTitle =
+    title && title.length > 50 ? `${title.slice(0, 50)}...` : title;
+  let movieName = name && name.length > 50 ? `${name.slice(0, 50)}...` : name;
+
   return (
     <GalleryItem>
       <MovieLink to={`/movies/${movieId}`}>
         <div>
-          <img src={`https://image.tmdb.org/t/p/w500/${url}`} alt={title} />
-          <h3>{title || name}</h3>
+          <img
+            src={url ? `https://image.tmdb.org/t/p/w500/${url}` : poster}
+            alt={title}
+          />
+          <h3>{movieTitle || movieName}</h3>
         </div>
       </MovieLink>
     </GalleryItem>
   );
 };
 
-// змінити
 MovieGalleryItem.propTypes = {
   movieId: PropTypes.number.isRequired,
   title: PropTypes.string,
