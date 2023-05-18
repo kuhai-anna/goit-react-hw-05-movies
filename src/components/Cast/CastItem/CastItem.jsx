834de2cca4545f9d502301f85d133cb89df4d876
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-18 04:25:36 +0300

    Update options for rendering CastItem

--- src/components/Cast/CastItem/CastItem.jsx
+++ src/components/Cast/CastItem/CastItem.jsx
@@ -1,9 +1,13 @@
 import PropTypes from 'prop-types';
+import unknownPerson from 'images/unknown-neon.jpeg';
 
 export const CastItem = ({ url, name, character }) => {
   return (
     <li>
-      <img src={`https://image.tmdb.org/t/p/w200/${url}`} alt={name} />
+      <img
+        src={url ? `https://image.tmdb.org/t/p/w200/${url}` : unknownPerson}
+        alt={name}
+      />
       <p>{name}</p>
       <p>Character: {character}</p>
     </li>
