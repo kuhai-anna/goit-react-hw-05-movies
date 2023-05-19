21d81eae7f03c1d42064d6b4a6b078888a9602c8
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-20 00:16:12 +0300

    Update SearchForm.jsx

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,4 +1,4 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { BsSearch } from 'react-icons/bs';
 import { IconContext } from 'react-icons';
 import { Formik } from 'formik';
@@ -19,7 +19,7 @@ export const SearchForm = ({ onSubmit }) => {
       return;
     }
 
-    // передача нового запиту оновлення сторінки до 1-ї
+    // передача нового запиту, оновлення сторінки до 1-ї
     onSubmit({
       ...values,
       page: 1,
@@ -53,54 +53,6 @@ export const SearchForm = ({ onSubmit }) => {
   );
 };
 
-SearchForm.propTypes = {};
-
-// export const Searchbar = ({ onSubmit }) => {
-//   // пошук зображення
-//   const searchImage = (values, { resetForm }) => {
-//     if (values.searchQuery.trim() === '') {
-//       alert('Enter a search query!');
-//       resetForm(); //якщо користувач ввів пробіли
-
-//       return;
-//     }
-
-//     // передача нового запиту оновлення сторінки до 1-ї
-//     onSubmit({
-//       ...values,
-//       page: 1,
-//     });
-
-//     resetForm();
-//   };
-
-//   return (
-//     <Header className="searchbar">
-//       <Formik initialValues={initialValues} onSubmit={searchImage}>
-//         <Form className="form">
-//           <IconContext.Provider
-//             value={{
-//               size: '20px',
-//             }}
-//           >
-//             <IconButton type="submit" aria-label="Search button">
-//               <BsSearch width="40" height="40" />
-//             </IconButton>
-//           </IconContext.Provider>
-
-//           <Field
-//             name="searchQuery"
-//             type="text"
-//             autoComplete="off"
-//             autoFocus
-//             placeholder="Search images and photos"
-//           />
-//         </Form>
-//       </Formik>
-//     </Header>
-//   );
-// };
-
-// Searchbar.propTypes = {
-//   onSubmit: PropTypes.func.isRequired,
-// };
+SearchForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
