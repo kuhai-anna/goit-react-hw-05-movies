e3e5f4363ba474a12e2e94ca07255326362a3e2c
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-20 00:15:55 +0300

    Update MovieGalleryItem.styled.js

--- src/components/MovieGallery/MovieGalleryItem/MovieGalleryItem.styled.js
+++ src/components/MovieGallery/MovieGalleryItem/MovieGalleryItem.styled.js
@@ -6,6 +6,7 @@ export const GalleryItem = styled.li`
 `;
 
 export const MovieLink = styled(NavLink)`
+  /* display: block; */
   width: 100%;
   height: 200px;
   min-height: 200px;
@@ -13,11 +14,13 @@ export const MovieLink = styled(NavLink)`
   border-radius: ${props => props.theme.spacing(1)};
   box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+  transform: scale(1);
 
   transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
-  :hover {
-    transform: scale(1.01);
-    cursor: zoom-in;
+  /* додати & ???? */
+  :hover,
+  :focus {
+    transform: scale(1.02);
   }
 `;
