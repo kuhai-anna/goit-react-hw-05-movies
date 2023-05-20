d53c427640b278f660ea0bcffdd9212c44948847
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-20 04:29:39 +0300

    Update MovieGalleryItem.styled.js

--- src/components/MovieGallery/MovieGalleryItem/MovieGalleryItem.styled.js
+++ src/components/MovieGallery/MovieGalleryItem/MovieGalleryItem.styled.js
@@ -6,21 +6,45 @@ export const GalleryItem = styled.li`
 `;
 
 export const MovieLink = styled(NavLink)`
-  /* display: block; */
-  width: 100%;
-  height: 200px;
-  min-height: 200px;
-  object-fit: cover;
+  display: block;
+  /* width: 100%; */
+  max-width: 320px;
   border-radius: ${props => props.theme.spacing(1)};
-  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
-    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+
+  text-decoration: none;
+  color: ${props => props.theme.colors.text};
+
+  background-color: #191919;
+
   transform: scale(1);
+  transition: transform, background-color,
+    box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
-  transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
-
-  /* додати & ???? */
-  :hover,
-  :focus {
+  &:hover,
+  &:focus {
     transform: scale(1.02);
+    box-shadow: 0px 0px 13px 0px #dfdfdf;
   }
+`;
+
+export const MovieWrap = styled.div`
+  width: 100%;
+  display: block;
+`;
+
+export const Poster = styled.img`
+  /* width: 100%; */
+  width: 320px;
+  height: 480px;
+
+  border-top-left-radius: ${props => props.theme.spacing(1)};
+  border-top-right-radius: ${props => props.theme.spacing(1)};
+
+  object-fit: cover;
+`;
+
+export const MovieName = styled.h3`
+  display: block;
+  padding: ${props => props.theme.spacing(2)};
+  height: 56px;
 `;
