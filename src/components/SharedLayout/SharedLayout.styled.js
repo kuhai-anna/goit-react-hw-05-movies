06849b8ef6b7aca02f8a4af14cd8a8e5be38097a
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-21 01:34:24 +0300

    Update SharedLayout.styled.js

--- src/components/SharedLayout/SharedLayout.styled.js
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -19,16 +19,21 @@ export const Link = styled(NavLink)`
   font-size: 16px;
   font-weight: 700;
   text-decoration: none;
+  text-transform: uppercase;
   color: ${props => props.theme.colors.text};
 
   transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
   &:not(:last-child) {
-    margin-right: ${props => props.theme.spacing(3)};
+    margin-right: ${props => props.theme.spacing(6)};
   }
 
   &:hover,
   &:focus {
+    color: ${props => props.theme.colors.textAccent};
+  }
+
+  &.active {
     color: ${props => props.theme.colors.textAccent};
   }
 `;
