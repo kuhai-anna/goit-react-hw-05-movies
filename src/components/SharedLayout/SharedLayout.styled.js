5c548259f38402fd55f34a13e5cff87e41b844ce
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-20 04:29:52 +0300

    Update SharedLayout.styled.js

--- src/components/SharedLayout/SharedLayout.styled.js
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -1,6 +1,12 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
+export const AppWraper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
 export const HeaderWrap = styled.div`
   display: flex;
   justify-content: space-between;
@@ -10,9 +16,19 @@ export const HeaderWrap = styled.div`
 export const Nav = styled.nav``;
 
 export const Link = styled(NavLink)`
+  font-size: 16px;
+  font-weight: 700;
   text-decoration: none;
+  color: ${props => props.theme.colors.text};
+
+  transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
   &:not(:last-child) {
     margin-right: ${props => props.theme.spacing(3)};
+  }
+
+  &:hover,
+  &:focus {
+    color: ${props => props.theme.colors.textAccent};
   }
 `;
