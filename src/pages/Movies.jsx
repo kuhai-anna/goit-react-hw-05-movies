0c51b4c9b9d86f5218d947cd035ea5b885d6e203
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-20 18:43:10 +0300

    Add search query validation

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { Button } from 'components/Button/Button';
 import { Loader } from 'components/Loader/Loader';
 import { MovieGallery } from 'components/MovieGallery/MovieGallery';
@@ -5,13 +7,11 @@ import { MovieGalleryErrorView } from 'components/MovieGallery/MovieGalleryError
 import { SearchForm } from 'components/SearchForm/SearchForm';
 import { Section } from 'components/Section/Section';
 import { Status } from 'constants/status';
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
 import { api } from 'services/movie-api';
 
 const Movies = () => {
   const [page, setPage] = useState(1);
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState();
   const [totalPages, setTotalPages] = useState();
   const [error, setError] = useState(null);
   const [status, setStatus] = useState(Status.IDLE);
@@ -19,7 +19,6 @@ const Movies = () => {
   const searchQuery = searchParams.get('query') ?? '';
   const notLastPage = page < Math.ceil(totalPages / 20);
 
-  // переробити
   const updateQueryString = params => {
     const nextParams = params.query !== '' ? { query: params.query } : {};
     setSearchParams(nextParams);
@@ -39,6 +38,12 @@ const Movies = () => {
           searchQuery,
           page
         );
+
+        if (results.length === 0) {
+          throw new Error(
+            'Sorry, there are no movies matching your search query'
+          );
+        }
 
         page === 1
           ? setMovies(results)
@@ -60,44 +65,30 @@ const Movies = () => {
     setPage(prevState => prevState + 1);
   };
 
-  // рендер компонентів в залежності від статусу
-  if (status === Status.IDLE) {
-    return (
+  return (
+    <>
       <Section>
         <SearchForm onSubmit={updateQueryString} />
       </Section>
-    );
-  }
 
-  if (status === Status.PENDING) {
-    return (
-      <Section>
-        <SearchForm onSubmit={updateQueryString} />
-        <Loader />;
-      </Section>
-    );
-  }
-
-  if (status === Status.REJECTED) {
-    return (
-      <Section>
-        <SearchForm onSubmit={updateQueryString} />
-        <MovieGalleryErrorView
-          message={`Whoops, something went wrong. ${error.message}. Please try again later.`}
-        />
-      </Section>
-    );
-  }
-
-  if (status === Status.RESOLVED) {
-    return (
-      <Section>
-        <SearchForm onSubmit={updateQueryString} />
-        <MovieGallery movies={movies} />
-        {notLastPage && <Button onClick={loadMoreBtnClick} />}
-      </Section>
-    );
-  }
+      {status === Status.PENDING ? (
+        <Section>
+          <Loader />
+        </Section>
+      ) : status === Status.REJECTED ? (
+        <Section>
+          <MovieGalleryErrorView
+            message={`Whoops, something went wrong. ${error.message}.`}
+          />
+        </Section>
+      ) : status === Status.RESOLVED ? (
+        <Section>
+          <MovieGallery movies={movies} />
+          {notLastPage && <Button onClick={loadMoreBtnClick} />}
+        </Section>
+      ) : null}
+    </>
+  );
 };
 
 export default Movies;
