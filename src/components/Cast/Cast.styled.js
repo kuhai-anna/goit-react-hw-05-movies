01ed4ce34cbb3966e09a5f89918d37566fe3de43
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-20 04:28:43 +0300

    Update Cast.styled.js

--- src/components/Cast/Cast.styled.js
+++ src/components/Cast/Cast.styled.js
@@ -1,6 +1,15 @@
 import styled from 'styled-components';
-import { Gallery } from 'components/MovieGallery/MovieGallery.styled';
+// import { Gallery } from 'components/MovieGallery/MovieGallery.styled';
 
-export const CastGallery = styled(Gallery)`
+export const CastGallery = styled.ul`
+  display: grid;
+  max-width: calc(100vw - 32px);
   grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
+  grid-gap: 16px;
+  margin-top: 0;
+  margin-bottom: 0;
+  padding: 0;
+  list-style: none;
+  margin-left: auto;
+  margin-right: auto;
 `;
