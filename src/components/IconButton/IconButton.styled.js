7b5cfa0cdff9810768ea6f6876c96a1edd0e39f8
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-21 01:28:57 +0300

    Update IconButton.styled.js

--- src/components/IconButton/IconButton.styled.js
+++ src/components/IconButton/IconButton.styled.js
@@ -8,18 +8,17 @@ export const Button = styled.button`
   height: 36px;
   outline: none;
   border: none;
-  border-radius: ${props => props.theme.spacing(1)};
-  background-color: ${props => props.theme.colors.bgdBtn};
+  border-radius: ${props => props.theme.spacing(0)};
+  background-color: transparent;
 
   color: ${props => props.theme.colors.icons};
 
-  transition-property: background-color, color;
+  transition-property: color;
   transition-duration: 250ms;
   transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
 
   &:hover,
   &:focus {
-    background-color: ${props => props.theme.colors.btnHover};
     color: ${props => props.theme.colors.iconsHover};
   }
 `;
