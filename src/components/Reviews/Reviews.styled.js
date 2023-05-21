266115fd45ea65f50eba2f82d4ba8c50192eeed8
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-21 05:22:41 +0300

    Create Reviews.styled.js

---
+++ src/components/Reviews/Reviews.styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const AlertMessage = styled.p`
+  padding: ${props => props.theme.spacing(6)} ${props => props.theme.spacing(0)};
+  text-align: center;
+`;
