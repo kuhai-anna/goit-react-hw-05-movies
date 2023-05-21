bd3db1e15ff456cccb776f09f9acd11e0dd6f82a
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-22 00:36:51 +0300

    Remove the error message background image

--- src/components/MovieGallery/MovieGalleryErrorView/MovieGalleryErrorView.styled.js
+++ src/components/MovieGallery/MovieGalleryErrorView/MovieGalleryErrorView.styled.js
@@ -1,24 +1,13 @@
 import styled from 'styled-components';
-import errorImage from 'images/sad-danbo-optim.jpeg';
-
-export const ErrorBox = styled.div`
-  width: 100%;
-  height: 83.6vh;
-  background-image: linear-gradient(rgba(0, 0, 0, 0.392), rgba(0, 0, 0, 0.392)),
-    url(${errorImage});
-  background-repeat: no-repeat;
-  background-position: start;
-  background-size: cover;
-`;
 
 export const ErrorText = styled.p`
-  position: relative;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  padding: ${props => props.theme.spacing(0)} ${props => props.theme.spacing(3)};
-
+  padding: ${props => props.theme.spacing(15)}
+    ${props => props.theme.spacing(2)};
   text-align: center;
-  font-size: 36px;
+  font-size: 24px;
   color: ${props => props.theme.colors.text};
+
+  @media screen and (min-width: 420px) {
+    font-size: 28px;
+  }
 `;
