48b4fd0d0327f722b5b5a7ea977bfd0a7fb8bf3f
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-21 05:22:44 +0300

    Update ReviewsItem.jsx

--- src/components/Reviews/ReviewsItem/ReviewsItem.jsx
+++ src/components/Reviews/ReviewsItem/ReviewsItem.jsx
@@ -1,11 +1,28 @@
 import PropTypes from 'prop-types';
+import {
+  AuthorName,
+  Review,
+  ReviewAuthorImg,
+  ReviewWrap,
+} from './ReviewsItem.styled';
+import unknownPerson from 'images/unknown-person.jpeg';
 
 export const ReviewsItem = ({ author, content }) => {
   return (
-    <li>
-      <p>Author: {author}</p>
+    <Review>
+      <ReviewWrap>
+        <ReviewAuthorImg
+          src={unknownPerson}
+          alt={`Author: ${author}`}
+          width={'60px'}
+        />
+        <p>
+          <AuthorName>Author: </AuthorName>
+          {author}
+        </p>
+      </ReviewWrap>
       <p>'{content}'</p>
-    </li>
+    </Review>
   );
 };
 
