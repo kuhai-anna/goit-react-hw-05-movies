7024cc643b6c6ec12da2f06fd1e16213997f14ca
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-22 01:44:50 +0300

    Remove searchParams

--- src/services/movie-api.js
+++ src/services/movie-api.js
@@ -1,73 +1,51 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.themoviedb.org/3/';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 const KEY = 'c87cc88294da77d4343c2f6d8278b44b';
 
 const fetchTrendingMovies = async (page, signal) => {
-  const searchParams = new URLSearchParams({
-    api_key: `${KEY}`,
-    page: `${page}`,
+  const { data } = await axios.get('/trending/all/day', {
+    params: { api_key: `${KEY}`, page },
+    signal,
   });
-
-  const { data } = await axios.get(
-    `${BASE_URL}trending/all/day?${searchParams}`,
-    { signal }
-  );
   return data;
 };
 
 const fetchMoviesWithQuery = async (searchQuery, page, signal) => {
-  const searchParams = new URLSearchParams({
-    api_key: `${KEY}`,
-    language: 'en-US',
-    query: `${searchQuery}`,
-    include_adult: 'false',
-    page: `${page}`,
-  });
-
-  const { data } = await axios.get(`${BASE_URL}search/movie?${searchParams}`, {
+  const { data } = await axios.get('/search/movie', {
+    params: {
+      api_key: `${KEY}`,
+      language: 'en-US',
+      query: `${searchQuery}`,
+      include_adult: 'false',
+      page,
+    },
     signal,
   });
   return data;
 };
 
 const fetchMovieDetails = async (moviId, signal) => {
-  const searchParams = new URLSearchParams({
-    api_key: `${KEY}`,
-    language: 'en-US',
+  const { data } = await axios.get(`/movie/${moviId}`, {
+    params: { api_key: `${KEY}`, language: 'en-US' },
     signal,
   });
-
-  const { data } = await axios.get(
-    `${BASE_URL}movie/${moviId}?${searchParams}`
-  );
   return data;
 };
 
 const fetchMovieCredits = async (moviId, signal) => {
-  const searchParams = new URLSearchParams({
-    api_key: `${KEY}`,
-    language: 'en-US',
+  const { data } = await axios.get(`movie/${moviId}/credits`, {
+    params: { api_key: `${KEY}`, language: 'en-US' },
+    signal,
   });
-
-  const { data } = await axios.get(
-    `${BASE_URL}movie/${moviId}/credits?${searchParams}`,
-    { signal }
-  );
   return data;
 };
 
 const fetchMovieReviews = async (moviId, page, signal) => {
-  const searchParams = new URLSearchParams({
-    api_key: `${KEY}`,
-    language: 'en-US',
-    page: `${page}`,
+  const { data } = await axios.get(`movie/${moviId}/reviews`, {
+    params: { api_key: `${KEY}`, language: 'en-US', page },
+    signal,
   });
-
-  const { data } = await axios.get(
-    `${BASE_URL}movie/${moviId}/reviews?${searchParams}`,
-    { signal }
-  );
   return data;
 };
 
